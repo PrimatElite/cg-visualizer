200b8b435e969c5a7c304c2ee374f85f1bb73dbe
LanskovNV <leins275@gmail.com>
2021-01-14 01:11:03 +0300

    Updated visualization drawing logic

--- app/src/Utils/draw.js
+++ app/src/Utils/draw.js
@@ -1,18 +1,36 @@
 import Polygon from '../Elements/Polygon';
 import { getByRef } from "./schema";
 
-const draw = (scene, data) => {
+function drawItem(scene, data, item) {
+    switch (item.type) {
+        case 'polygon':
+            const points = getByRef(data, item.src.$ref);
+            const polygon = new Polygon(points);
+            scene.add(polygon.draw());
+            break;
+        case 'paints':
+            break;
+        default:
+            console.log('Unsupported visualization type');
+    }
+}
+
+function drawVisualizations(scene, data) {
     data.visualizations.forEach(item => {
-        switch (item.type) {
-            case 'polygon':
-                const points = getByRef(data, item.src.$ref);
-                const polygon = new Polygon(points);
-                scene.add(polygon.draw());
-                break;
-            default:
-                console.log('Unsupported visualization type');
-        }
+        draw(scene, data, item);
     });
 }
 
-export default draw;
+const draw = (scene, data, item) => {
+    if (!item.type) {
+        item = getByRef(data, item.$ref);
+    }
+
+    if ((typeof item) === 'object') {
+        drawItem(scene, data, item);
+    } else {
+        item.forEach(el => draw(scene, data, el));
+    }
+}
+
+export default drawVisualizations;
